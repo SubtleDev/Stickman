347cdd1f7c6f7e5db8668e8aca98b4b2d4563891
BuildTools <unconfigured@null.spigotmc.org>
2015-02-14 09:23:22 +0100

    Updated version for release

--- Stickman/src/me/subtleoliver/stickmandrawer/Stickman.java
+++ Stickman/src/me/subtleoliver/stickmandrawer/Stickman.java
@@ -13,7 +13,7 @@ import javax.swing.border.TitledBorder;
 
 public class Stickman extends JFrame implements ActionListener {
 
-	public final String VERS = "1.1.0"; //Version number
+	public final String VERS = "1.0.0"; //Version number
 	public final String TITLE = "Stickman " + VERS; //Frame Title
 	private static final long serialVersionUID = -926469678696297356L; //SerialVersionUID
 	//Stuff initialization
